e80658867fde31b07ff4ca1c518a11d24f940cf8
Alan Kha <akhahaha@gmail.com>
2015-11-09 16:05:34 -0800

    Rename project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Biocatalog' });
+  res.render('index', { title: 'Aztec Retrieval' });
 });
 
 module.exports = router;
