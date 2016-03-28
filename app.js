a364e4aa6dfe4a2665b97e037a4db21cf94a68b6
Alan Kha <akhahaha@gmail.com>
2016-03-28 15:11:58 -0700

    Implement update executable module

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/update', routes);
 app.use('/biocatalog', biocatalog);
 app.use('/bioconductor', bioconductor);
 app.use('/biojs', biojs);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var updater = require('../update.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Aztec Retrieval' });
+});
+
+router.get('/update', function(req, res, next) {
+  updater.update();
+  res.send("Updating all resources.");
 });
 
 module.exports = router;
