352b55a901916e0c0b31f208cab682e99e631049
Avirudh Theraja <avirudhtheraja@gmail.com>
2016-09-14 15:39:51 -0700

    Fixed args bug in getting_started.sh

--- aztec_extraction_classification/scripts/getting_started.sh
+++ aztec_extraction_classification/scripts/getting_started.sh
@@ -5,4 +5,4 @@ mkdir  $PWD/../slots-extraction/data/$1/textExtracts
 mkdir  $PWD/../slots-extraction/data/$1/XMLExtracts
 
 python $PWD/../slots-extraction/aztec_extraction_classification/scripts/pdf_extract.py $PWD/../slots-extraction/data/$1/ $PWD/../slots-extraction/data/$1/XMLExtracts/ $PWD/../slots-extraction/data/$1/textExtracts/
-python $PWD/../slots-extraction/aztec_extraction_classification/scripts/parse_extracts.py $PWD/../slots-extraction/data/$1/XMLExtracts/ $PWD/../slots-extraction/data/$1/textExtracts/ None $PWD/../slots-extraction/data/$1/slotExtracts/slot_extracts.json $2
+python $PWD/../slots-extraction/aztec_extraction_classification/scripts/parse_extracts.py $PWD/../slots-extraction/data/$1/XMLExtracts/ $PWD/../slots-extraction/data/$1/textExtracts/ $PWD/../slots-extraction/data/$1/slotExtracts/slot_extracts.json $2
